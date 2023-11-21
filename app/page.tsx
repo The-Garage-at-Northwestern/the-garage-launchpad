1c8a4e1b46f29134aacd61b29187db80869350d7
Jason <jasonlu2025@u.northwestern.edu>
2023-11-21 16:20:21 -0600

    removed svg placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         </div>
 
         {/* Landing Copy */}
-        <div className="w-1/2 top-8 left-8 relative">
+        <div className="w-2/5 top-8 left-8 relative">
           <h1 className="text-7xl font-bold text-white">IDEA TO CREATION</h1>
           <h1 className="text-7xl font-bold text-white">
             {" "}
@@ -31,90 +31,6 @@ export default function Home() {
           </h1>
         </div>
       </section>
-
-      {/* SVG square in top-right hand corner */}
-      <svg
-        className="absolute top-8 right-2 z-[-1]"
-        width="100"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Top right rectangle */}
-        <rect
-          x="0"
-          y="0"
-          width="75"
-          height="75"
-          rx="0"
-          stroke="#FFFFFF"
-          fill="#FFFFFF"
-        />
-      </svg>
-
-      {/* SVG square in bottom-right hand corner */}
-      <svg
-        className="absolute bottom-4 right-12 z-[-1]"
-        width="200"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Bottom right rectangle */}
-        <rect
-          x="0.5"
-          y="0.5"
-          width="200"
-          height="50"
-          rx="0"
-          stroke="#000000"
-          fill="#FFFFFF"
-        />
-      </svg>
-
-      {/* SVG square in bottom-left hand corner */}
-      <svg
-        className="absolute bottom-4 left-2 z-[-1]"
-        width="200"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Bottom left rectangle */}
-        <rect
-          x="0.5"
-          y="0.5"
-          width="200"
-          height="50"
-          rx="0"
-          stroke="#FFFFFF"
-          fill="#000000"
-        />
-      </svg>
-
-      {/* SVG square in bottom-left hand corner */}
-      <svg
-        className="absolute bottom-4 left-40 z-[-1]"
-        width="200"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Bottom left rectangle */}
-        <rect
-          x="0.5"
-          y="0.5"
-          width="200"
-          height="50"
-          rx="0"
-          stroke="#000000"
-          fill="#FFFFFF"
-        />
-      </svg>
 
       {/* ############################################################################## */}
       {/* A separate section below the carousels for explanation of the program */}
